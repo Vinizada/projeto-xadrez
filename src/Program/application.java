8a5152a751d5e36563fdb362f532c037b3d34f75
Vinicius Lima <vinislimadasilva17@gmail.com>
2021-06-08 20:03:08 -0300

    Class Position

---
+++ src/Program/application.java
@@ -0,0 +1,16 @@
+package Program;
+
+import boardgame.Position;
+
+public class application {
+
+	public static void main(String[] args) {
+	
+		
+		Position pos = new Position(3, 5);
+
+		System.out.println(pos);	
+		
+	}
+
+}
